e617b993b235d5bf960072c159769594e2f3fbbe
gadname <gakugaku1114@icloud.com>
2025-03-09 18:24:32 +0900

    🔨:ulidの設定

--- backend/fastapi/app/models/chat_bot.py
+++ backend/fastapi/app/models/chat_bot.py
@@ -1,16 +1,22 @@
 from app.db.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import DateTime, VARCHAR
+from app.utils.id_generator import generate_ulid
 
 
 class ChatBot(Base):
     __tablename__ = "chat_bots"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
-    color: Mapped[str] = mapped_column(String(255), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now)
+    id: Mapped[int] = mapped_column(
+        VARCHAR(26),
+        index=True,
+        nullable=False,
+        unique=True,
+        default=generate_ulid,
+        primary_key=True,
+    )
+    name: Mapped[str] = mapped_column(VARCHAR(255), nullable=False)
+    color: Mapped[str] = mapped_column(VARCHAR(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
